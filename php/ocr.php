99d2db6ed4bd0285c15c93c4983ff6f543118ff7
Nat Kanthan <natkeeran@gmail.com>
2016-02-12 21:25:39 -0500

    remove uncessary comments

--- php/ocr.php
+++ php/ocr.php
@@ -5,14 +5,10 @@ require  "createConfig.php";
 $pdfURL = $_POST['pdfUrl'];
 $numOfColumns = $_POST['numOfColumns'];
 
-//echo "PDF URL " . $pdfURL . " ";
-//echo "Number of Columns " . $numOfColumns . " ";
+// Create the config.ini
 $configPath = "/usr/local/bin/config.ini";
-
 createConfigFile("/usr/local/bin/config.ini", $pdfURL, $numOfColumns);
 
-
-//$command = escapeshellcmd('python /usr/local/bin/do_ocr.py');
 $command = "python /usr/local/bin/do_ocr.py";
 $output = shell_exec($command);
 //echo $output;
